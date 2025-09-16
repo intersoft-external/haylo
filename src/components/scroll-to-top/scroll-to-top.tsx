0fac3785a1bea8e3462e2c6591d018825e39e1c6
abarua967 <abarua967@gmail.com>
2025-09-16 16:27:15 +0530

    modify UI

--- src/components/scroll-to-top/scroll-to-top.tsx
+++ src/components/scroll-to-top/scroll-to-top.tsx
@@ -1,5 +1,6 @@
 import { ArrowUp } from "lucide-react";
 import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function ScrollToTop() {
     const [visible, setVisible] = useState(false);
@@ -22,13 +23,20 @@ export default function ScrollToTop() {
     };
 
     return (
-        visible && (
-            <button
-                onClick={scrollToTop}
-                className="fixed cursor-pointer bottom-6 right-6 p-3 rounded-full bg-gradient-to-r from-orange-400 to-pink-500 text-white shadow-lg hover:scale-110 transition-transform"
-            >
-                <ArrowUp className="w-5 h-5" />
-            </button>
-        )
+        <AnimatePresence>
+            {visible && (
+                <motion.button
+                    onClick={scrollToTop}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 20 }}
+                    whileHover={{ scale: 1.2, rotate: 10 }}
+                    whileTap={{ scale: 0.95, rotate: 0 }}
+                    className="fixed bottom-20 cursor-pointer right-6 p-3 rounded-full bg-gradient-to-r from-orange-400 to-pink-500 text-white shadow-lg"
+                >
+                    <ArrowUp className="w-5 h-5" />
+                </motion.button>
+            )}
+        </AnimatePresence>
     );
 }
